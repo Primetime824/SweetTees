b94df43c5513e2afb8e2114d668314de4a08e5ad
Primetime824 <60769372+Primetime824@users.noreply.github.com>
2024-07-20 18:19:03 -0500

    Added javascript page, added pages for each navigation tab,

---
+++ JavaScript/myscripts.js
@@ -0,0 +1,9 @@
+document.querySelectorAll(".navList").forEach((ele) =>
+  ele.addEventListener("click", function (event) {
+    event.preventDefault();
+    document
+      .querySelectorAll(".navList")
+      .forEach((ele) => ele.classList.remove("active"));
+    this.classList.add("active")
+  })
+);
